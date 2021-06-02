b98740ff6246ec308f969430a1f61b07db13066c
rladbgus <cocokiuuu1858@gmail.com>
2021-06-02 11:40:50 +0900

    _app 레이아웃 세팅

---
+++ pages/detail.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const DetailPage = () => (
+  <>
+    <h1>상세페이지</h1>
+    <p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </p>
+  </>
+);
+
+export default DetailPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
-import Link from 'next/link';
-import Layout from '../components/Layout';
+import React from 'react';
+import Head from 'next/head';
+import Home from '../components/Home';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <>
+    <Head>
+      <title>Main</title>
+    </Head>
+    <Home />
+  </>
 );
-
 export default IndexPage;
