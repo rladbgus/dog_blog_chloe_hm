ba0b00e43e81f1d9746b987e61db95d159a699b0
rladbgus <cocokiuuu1858@gmail.com>
2021-06-29 10:25:12 +0900

    환경변수 사용을 위한 next.config 세팅 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-// const withTypescript = require('@zeit/next-typescript');
-// const path = require('path');
+const withTypescript = require('@zeit/next-typescript');
+const path = require('path');
 
-// module.exports = withTypescript({
-//   env: {
-//     EXP: process.env.EXP
-//   }
-// });
+module.exports = withTypescript({
+  env: {
+    EXP: process.env.EXP
+  }
+});
