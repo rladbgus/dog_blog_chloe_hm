b3888642e53d1b019fb5cfb858fdbacbd90a74bd
chloe <chloe@hellomarket.com>
2021-11-10 16:32:39 +0900

    주석 정리

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = `https://api.thedogapi.com/v1`;
 const DOG_DATA_API_KEY = process.env.NEXT_PUBLIC_DOG_DATA_API_KEY;
 
-// Header 데이터
+// Header Data
 export default axios.create({
   baseURL: baseURL,
   timeout: 5000,
